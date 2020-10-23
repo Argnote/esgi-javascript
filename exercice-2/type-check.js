de47ec3015e16ce07aff3a9539b2c6511aa2ef42
Ewan LEMEE <ewan92@laposte.net>
2020-10-23 18:09:53 +0200

    modif return type check v1

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,8 +1,6 @@
 function type_check_v1(value,type)
 {
-    if(typeof(value)===type)
-        return true;
-    return false;
+    return (typeof(value)===type)
 }
 
 // console.log(type_check_v1("ttt","number"));
